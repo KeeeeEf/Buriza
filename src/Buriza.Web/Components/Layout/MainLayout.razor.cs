7880f93645675a9a5e65aeb25c95e4a476b7c5bd
KeeeeEf <kfrgallego@gmail.com>
2025-04-30 22:16:01 +0800

    feat(web): added background image and fix missing styling

--- src/Buriza.Web/Components/Layout/MainLayout.razor.cs
+++ src/Buriza.Web/Components/Layout/MainLayout.razor.cs
@@ -15,18 +15,26 @@ public partial class MainLayout
         {
             Background = "#030303",
             AppbarBackground = "#0E0E0E",
+            BackgroundGray = "#2B2B4A",
+            Dark = "#2C2D42",
+            DarkLighten = "#2C2D42",
             Primary = "#227CFF",
             TextPrimary = "#E8EFFB",
             TextSecondary = "#838C9E",
+            Divider = "#2C2D42",
             Success = "#7BFFB2"
         },
         PaletteLight = new()
         {
             Background = "#E8EFFB",
             AppbarBackground = "#EEEEEE",
+            Dark = "#2C2D4226",
+            DarkLighten = "#E8EFFB",
             Primary = "#227CFF",
             TextPrimary = "#445671",
-            TextSecondary = "#7E8EA5"
+            TextSecondary = "#7E8EA5",
+            Divider = "#FFFFFF",
+            Success = "#7BFFB2"
         },  
         Typography = new()
         {
